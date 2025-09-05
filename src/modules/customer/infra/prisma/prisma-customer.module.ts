6d53d70e451554e712cdb10fc0ec2dc273a3937f
Jose Junior <juniorferreira1919@hotmail.com>
2025-09-04 21:49:34 -0300

    feat: add prisma customer module

---
+++ src/modules/customer/infra/prisma/prisma-customer.module.ts
@@ -0,0 +1,26 @@
+import { BusinessCustomerRepository } from '@modules/customer/domain/repositories/Business-customer.repository';
+import { IndividualCustomerRepository } from '@modules/customer/domain/repositories/Individual-customer.repositories';
+import { Module } from '@nestjs/common';
+import { DbModule } from '@shared/database/database.module';
+
+import { PrismaBusinessCustomerRepository } from './repositories/prisma-business-customer.repository';
+import { PrismaIndividualCustomerRepository } from './repositories/prisma-individual-customer.repository';
+
+@Module({
+  imports: [DbModule],
+  providers: [
+    {
+      provide: IndividualCustomerRepository,
+      useClass: PrismaIndividualCustomerRepository,
+    },
+    {
+      provide: BusinessCustomerRepository,
+      useClass: PrismaBusinessCustomerRepository,
+    },
+  ],
+  exports: [
+    PrismaIndividualCustomerRepository,
+    PrismaBusinessCustomerRepository,
+  ],
+})
+export class PrismaCustomerModule {}
